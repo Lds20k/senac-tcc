3b08c7ddff04c9913910c5b0789e93e480d16388
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2023-10-13 00:41:04 -0300

    feat: test countour-based

--- src/test/countour_test.py
+++ src/test/countour_test.py
@@ -10,55 +10,72 @@ def numpytoimage(numpy):
 
 if __name__ == '__main__':
     reference = cv2.imread("output_croped.png",0)
-    _, thresh_ref = cv2.threshold(reference, 75, 255, 0)
 
     extract = cv2.imread("output_2d.png")
 
     height = extract.shape[0]
     width = extract.shape[1]
-
     image = Image.new('RGB', (width, height))
 
-    flag = False
     for x_in in range(width):
         for y_in in range(height):
-            pixel_color = extract[y_in, y_in]
+            pixel_color = extract[y_in, x_in]
 
-            if not flag:
-                print(pixel_color)
-                flag = True
-
-            if pixel_color[0] == 255 and pixel_color[1] ==  191 and pixel_color[2] == 0:
-                image.putpixel((x_in, y_in), (255, 255, 255))
-            else:
+            if pixel_color[0] == 255 and pixel_color[1] == 191 and pixel_color[2] == 0:
                 image.putpixel((x_in, y_in), (0, 0, 0))
+            else:
+                image.putpixel((x_in, y_in), (255, 255, 255))
 
-    open_cv_image = np.array(image) 
-    # Convert RGB to BGR 
-    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    open_cv_image = np.array(image)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
     gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
 
-    _, thresh_extract = cv2.threshold(open_cv_image, 75, 255, 0)
+    cv2.imwrite("map_mas.png", gray)
+
+    # lut = [[  1,  1,  1],
+    #     [  1,  0,  0],
+    #     [  0,  1,  0],
+    #     [0.5,0.5,0.5]]
+
+    # _, thresh_ref = cv2.threshold(reference, 75, 1, 0)
+    # _, thresh_extract = cv2.threshold(gray, 75, 2, 0)
+    # C = lut[thresh_ref + thresh_extract]
 
 
+    _, thresh_ref = cv2.threshold(reference, 75, 255, 0)
+    _, thresh_extract = cv2.threshold(gray, 75, 255, 0)
     C = np.zeros(shape=(len(thresh_ref), len(thresh_ref[0]), 3))
+
+    counter_correct = 0
+    counter_correct_and_errors = 0
 
     for i in range (0, thresh_ref.shape[0],1):
         for j in range(0, thresh_ref.shape[1], 1):
-            if np.all(thresh_ref[i][j] == thresh_extract[i][j]) and thresh_ref[i][j] == 0:
-                C[i][j] = 1
-            elif thresh_ref[i][j] == 0:
-                C[i][j][0] = 0
-                C[i][j][1] = 1
-                C[i][j][2] = 0
-            elif np.any(thresh_extract[i][j]) == 0:
-                C[i][j][0] = 1
-                C[i][j][1] = 0
-                C[i][j][2] = 0
-            else:
+            if thresh_ref[i][j] == thresh_extract[i][j] and thresh_ref[i][j] == 0:
+                # sem nada nos dois
+
                 C[i][j][0] = 0.5
                 C[i][j][1] = 0.5
                 C[i][j][2] = 0.5
+            elif thresh_ref[i][j] == 0:
+                C[i][j][0] = 1
+                C[i][j][1] = 0
+                C[i][j][2] = 0
+                counter_correct_and_errors += 1
+            elif np.any(thresh_extract[i][j]) == 0:
+                C[i][j][0] = 0
+                C[i][j][1] = 1
+                C[i][j][2] = 0
+                counter_correct_and_errors += 1
+            else:
+                # igual nos dois
+                counter_correct += 1
+                counter_correct_and_errors += 1
+                C[i][j] = 1
+
 
     C_image = numpytoimage(C)
     C_image.save("quality.png")
+
+    result = (counter_correct / counter_correct_and_errors) * 100
+    print(f'O resultado eh {result}%')
